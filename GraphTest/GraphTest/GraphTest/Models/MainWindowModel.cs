4d351f68434034e0f2d0eb3eb90a5991a23f9565
Bart De Meyer <bart@bartdemeyer.be>
2013-03-01 09:13:48 +0100

    Added GraphTest

---
+++ GraphTest/GraphTest/GraphTest/Models/MainWindowModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using ZGroup.MonitoringSuite.BDO;
+using ZGroup.MonitoringSuite.BDO.Client;
+
+namespace GraphTest.Models
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<GasDetectorFake> gasDetectors;
+
+        public ObservableCollection<GasDetectorFake> GasDetectors
+        {
+            get { return gasDetectors; }
+            set
+            {
+                gasDetectors = value;
+                OnPropertyChanged("GasDetectors");
+            }
+        }
+
+
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        #endregion
+
+    }
+
+    public class GasDetectorFake
+    {
+        public long GasDetectorId { get; set; }
+        public decimal Value { get; set; }
+    }
+}
